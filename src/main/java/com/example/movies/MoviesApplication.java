ce3593ea2fde415a17cf482674a8f2445cab352b
ImeshaDilshani <imeshadilshani212@gmail.com>
2023-12-13 09:20:55 +0530

    writing the first end point

--- src/main/java/com/example/movies/MoviesApplication.java
+++ src/main/java/com/example/movies/MoviesApplication.java
@@ -2,12 +2,19 @@ package com.example.movies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class MoviesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MoviesApplication.class, args);
 	}
-	//fjndjgn
+	@GetMapping
+	public String apiRoot(){
+		return "Hello, World";
+	}
+
 }

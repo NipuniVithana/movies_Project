50f4f261de584840d20937970e8b2c1943adc35b
Nipuni Vithana <ndhananja99@gmail.com>
2023-12-26 12:54:12 +0530

    Setting up environment variables Movies and Reviews Endpoints

--- src/main/java/com/example/movies/Review.java
+++ src/main/java/com/example/movies/Review.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection="reviews")
@@ -11,7 +12,11 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Review {
+    @Id
     private ObjectId id;
     private String body;
 
+    public Review(String body) {
+        this.body = body;
+    }
 }
---
+++ src/main/java/com/example/movies/ReviewRepository.java
@@ -0,0 +1,10 @@
+package com.example.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+
+}
